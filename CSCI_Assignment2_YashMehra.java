514c2430c603d698a5cf1099015fc13e62882198
yash8mehra <yash8mehra@gmail.com>
2026-01-29 20:17:46 -0500

    idk what im doing

--- CSCI_Assignment2_YashMehra.java
+++ CSCI_Assignment2_YashMehra.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class CSCI_Assignment2_YashMehra{
     public static void main(String args[]){
-        System.out.println("Hi just starting this");
+        CSCI_Assignment2_YashMehra fraction = new CSCI_Assignment2_YashMehra();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your numerator");
+        int numerator = scanner.nextInt();
+
+        System.out.println("Enter your denominator");
+        int denominator = scanner.nextInt();
+
+        
+
+        
 
 
 
-
+    scanner.close();
     }
 
 
--- CSCI_Fraction_YashMehra.java
+++ CSCI_Fraction_YashMehra.java
@@ -1,13 +1,26 @@
-public class CSCI_Fraction_YashMehra{
-    public static void main(String args[]){
-        System.out.println("This is the fraction class");
+class CSCI_Fraction_YashMehra{
+    private int numerator;
+    private int denominator;
 
 
-
-
-    }
-
-
+    //things to do from now on
+    //make fraction numer over denom
+    //Simplify the fraction 
+    //gcd
+    //adding subtracting multiplying division methods
+    //toString() method
+    //raised to power method (pow), negate method (negative)
+    //then main to test 
 
 
 }
+
+
+
+
+
+
+
+
+
+
