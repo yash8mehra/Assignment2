e1328b381a365130df92cc5255ab4bda6d067f44
yash8mehra <yash8mehra@gmail.com>
2026-02-03 13:40:38 -0500

    Made good progress

--- CSCI_Fraction_YashMehra.java
+++ CSCI_Fraction_YashMehra.java
@@ -2,14 +2,86 @@ class CSCI_Fraction_YashMehra{
     private int numerator;
     private int denominator;
 
+    
 
+    
     //things to do from now on
     //make fraction numer over denom
-    //Simplify the fraction 
-    //gcd
-    //adding subtracting multiplying division methods
+    public CSCI_Fraction_YashMehra(){
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+
+    //getters
+    
+
     //toString() method
+    public String toString(int numerator, int denominator){
+    if ((numerator == 0)&&(denominator == 0)){
+        return "NaN";
+    } else if (denominator == 0){
+        if (numerator > 0){
+            return "Infinity";
+        } else {
+            return "-Infinity";
+        }
+    }
+    String strNumer = "" + numerator;
+    String strDenom = "" + denominator;
+    if (denominator == 1){
+        return strNumer;
+    }
+    return strNumer+"/"+strDenom;
+
+    }
+    //Simplify the fraction 
+    public void simplifyFraction(){
+
+    }
+    //gcd
+    public int greatestCommonDivisor(int a, int b){
+        if ( a < 0 ){
+            negate(a);}
+        int remainder = 0;
+
+            while (b != 0){
+                remainder = a % b;
+                a = b;
+                b = remainder;
+            }
+                if (a == 0){
+                    a = 1;
+                    return 
+                }
+    }
+    //adding subtracting multiplying division methods
+    public CSCI_Fraction_YashMehra add(){
+
+
+    }
+    public CSCI_Fraction_YashMehra subtract(){
+
+
+    }
+    public CSCI_Fraction_YashMehra multiply(){
+
+
+
+    }
+    public CSCI_Fraction_YashMehra divide(){
+
+
+
+    }
     //raised to power method (pow), negate method (negative)
+    public CSCI_Fraction_YashMehra negate(){
+
+
+    }
+    public CSCI_Fraction_YashMehra pow(){
+
+
+    }
     //then main to test 
 
 
