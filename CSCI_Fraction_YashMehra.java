f6f436c031c9c211f61775e60d6528233d3fe7f4
yash8mehra <yash8mehra@gmail.com>
2026-02-05 22:00:11 -0500

    finished the core

--- CSCI_Fraction_YashMehra.java
+++ CSCI_Fraction_YashMehra.java
@@ -9,18 +9,24 @@ class CSCI_Fraction_YashMehra{
     //make fraction numer over denom
     //for x/y fractions
     public CSCI_Fraction_YashMehra(long numer, long denom){
-
+        if (numer < 0 && denom < 0){ //both negative
+            numer = -numer;
+            denom = -denom;
+        } else if (denom < 0){ //only denom negative
+            numer = -numer;
+            denom = -denom;
+        }
         //simplify the fraction first
 
         long gcd = greatestCommonDivisor(numer, denom);
 
-        this.numerator = numer / gcd;
-        this.denominator = denom / gcd;
+        numerator = numer / gcd;
+        denominator = denom / gcd;
     }
     //for x/1 fractions
     public CSCI_Fraction_YashMehra(long numer){
-        this.numerator = numer;
-        this.denominator = 1;
+        numerator = numer;
+        denominator = 1;
     }
     //toString() method
     public String toString(){
@@ -61,59 +67,59 @@ class CSCI_Fraction_YashMehra{
     }
     //adding subtracting multiplying division methods
     public CSCI_Fraction_YashMehra add(CSCI_Fraction_YashMehra fractionadd){
-        long numer = (this.numerator * fractionadd.denominator) + (fractionadd.numerator * this.denominator);
-        long denom = this.denominator * fractionadd.denominator;
+        long numer = (numerator * fractionadd.denominator) + (fractionadd.numerator * denominator);
+        long denom = denominator * fractionadd.denominator;
         return new CSCI_Fraction_YashMehra(numer, denom);
 
     }
     public CSCI_Fraction_YashMehra subtract(CSCI_Fraction_YashMehra fractionsub){
-        long numer = (this.numerator * fractionsub.denominator) - (fractionsub.numerator * this.denominator);
-        long denom = this.denominator * fractionsub.denominator;
+        long numer = (numerator * fractionsub.denominator) - (fractionsub.numerator * denominator);
+        long denom = denominator * fractionsub.denominator;
         return new CSCI_Fraction_YashMehra(numer, denom);
 
 
     }
     public CSCI_Fraction_YashMehra multiply(CSCI_Fraction_YashMehra fractionmult){
-        long numer = (this.numerator * fractionmult.numerator);
-        long denom = (this.denominator * fractionmult.denominator);
+        long numer = (numerator * fractionmult.numerator);
+        long denom = (denominator * fractionmult.denominator);
         return new CSCI_Fraction_YashMehra(numer,denom);
 
 
     }
     public CSCI_Fraction_YashMehra divide(CSCI_Fraction_YashMehra fractionDiv){
-        long numer = this.numerator * fractionDiv.denominator;
-        long denom = this.denominator * fractionDiv.numerator;
+        long numer = numerator * fractionDiv.denominator;
+        long denom = denominator * fractionDiv.numerator;
         return new CSCI_Fraction_YashMehra(numer,denom);
 
 
     }
     //raised to power method (pow), negate method (negative)
     public CSCI_Fraction_YashMehra negate(){
-            return new CSCI_Fraction_YashMehra(-1 * this.numerator, this.denominator);
+            return new CSCI_Fraction_YashMehra(-1 * numerator, denominator);
 
     }
     public CSCI_Fraction_YashMehra pow(int exp){
         if (exp == 0){
             return new CSCI_Fraction_YashMehra(1); //anything to the zero power is 1
-        } else {
-            if (exp > 0) {
-                long numer = numerator;
-                long denom = denominator;
-                for (int i = 0; i < exp; i++){
-                    numer *= numerator;
-                    denom *= denominator;
-                }
-            } else if(exp < 0) {
-                long numer = denominator;
-                long denom = numerator;
-                for (int i = 0; i < exp; i++){
-                    numer *= denominator;
-                    denom *= numerator;
-                }
+        } else if (exp > 0) {
+            long numer = numerator;
+            long denom = denominator;
+            for (int i = 1; i < exp; i++){
+                numer *= numerator;
+                denom *= denominator;
+            }
+                return new CSCI_Fraction_YashMehra(numer, denom);
+        } else{ //negative exponent
+            long numer = denominator;
+            long denom = numerator;
+            for (int i = 1; i < (-1*exp); i++){
+                numer *= denominator;
+                denom *= numerator;
+            }
+            return new CSCI_Fraction_YashMehra(numer, denom);
         }
+            
     }
-    //then main to test 
-
 
 }
 
